f67e8177b4dcec58852d15ca9cc65e7bce33ece6
abdusattar <git@github.com:Abdusattar2005/Pg.git>
2024-11-17 22:54:06 +0600

    шаблон

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\Main\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Admin', 'prefix'=>'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+    Route::get('/', [IndexController::class, 'index'])->name('main.index');
+    });
 });
