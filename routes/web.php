4dad988ec7b570ba9899e26b3ae348f71944ca2c
abdusattar <git@github.com:Abdusattar2005/Pg.git>
2024-11-20 22:19:26 +0600

    login -+-+

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,15 @@ Route::group(['namespace' => 'Main'], function () {
 Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => ['auth',  'verified']], function () {
     Route::get('/', [IndexController::class, 'index'])->name('admin.main.index');
 
-    Route::resource('user', UserController::class)->names([
-        'index' => 'admin.user.index',
-        'create' => 'admin.user.create',
-        'store' => 'admin.user.store',
-        'show' => 'admin.user.show',
-        'edit' => 'admin.user.edit',
-        'update' => 'admin.user.update',
-        'destroy' => 'admin.user.destroy',
-    ]);
+    Route::group(['namespace' => 'User', 'prefix' => 'users'], function () {
+        Route::get('/', [UserController::class, 'index'])->name('admin.user.index');
+        Route::get('/create', [UserController::class, 'create'])->name('admin.user.create');
+        Route::post('/', [UserController::class, 'store'])->name('admin.user.store');
+        Route::get('/{user}', [UserController::class, 'show'])->name('admin.user.show');
+        Route::get('/{user}/edit', [UserController::class, 'edit'])->name('admin.user.edit');
+        Route::patch('/{user}', [UserController::class, 'update'])->name('admin.user.update');
+        Route::delete('/{user}', [UserController::class, 'destroy'])->name('admin.user.destroy');
+    });
 });
 
 Auth::routes();
