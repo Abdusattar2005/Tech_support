ea82fe683989154346af14f57106737349b02b3a
abdusattar <git@github.com:Abdusattar2005/Pg.git>
2024-12-09 01:42:06 +0600

    Like and Dislike

--- resources/views/personal/liked/index.blade.php
+++ resources/views/personal/liked/index.blade.php
@@ -3,7 +3,7 @@
 <div class="content-wrapper">
     <section class="content-header">
         <h1>
-            Понравившиеся посты
+            Понравившиеся видео
         </h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-home"></i>Главная</a></li>
