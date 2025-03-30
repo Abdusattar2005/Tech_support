c82945ad31835c2eb0e3144fe85e79894595fcc4
abdusattar <git@github.com:Abdusattar2005/Pg.git>
2025-03-30 22:54:33 +0600

    push--

--- routes/web.php
+++ routes/web.php
@@ -75,17 +75,17 @@ Route::group(['namespace' => 'Personal', 'prefix' => 'personal', 'middleware' =>
     });
 });
 
-Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => ['auth', 'verified']], function () {
-    Route::get('/', [\App\Http\Controllers\Admin\Main\IndexController::class, 'index'])->name('admin.main.index');
+    Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => ['auth', 'verified']], function () {
+        Route::get('/', [\App\Http\Controllers\Admin\Main\IndexController::class, 'index'])->name('admin.main.index');
 
-    Route::group(['namespace' => 'User', 'prefix' => 'users'], function () {
-        Route::get('/', [UserController::class, 'index'])->name('admin.user.index');
-        Route::get('/create', [UserController::class, 'create'])->name('admin.user.create');
-        Route::post('/store', [UserController::class, 'store'])->name('admin.user.store');
-        Route::get('/{user}', [UserController::class, 'show'])->name('admin.user.show');
-        Route::get('/{user}/edit', [UserController::class, 'edit'])->name('admin.user.edit');
-        Route::patch('/{user}', [UserController::class, 'update'])->name('admin.user.update');
-        Route::delete('/{user}', [UserController::class, 'delete'])->name('admin.user.delete');
+        Route::group(['namespace' => 'User', 'prefix' => 'users'], function () {
+            Route::get('/', [UserController::class, 'index'])->name('admin.user.index');
+            Route::get('/create', [UserController::class, 'create'])->name('admin.user.create');
+            Route::post('/store', [UserController::class, 'store'])->name('admin.user.store');
+            Route::get('/{user}', [UserController::class, 'show'])->name('admin.user.show');
+            Route::get('/{user}/edit', [UserController::class, 'edit'])->name('admin.user.edit');
+            Route::patch('/{user}', [UserController::class, 'update'])->name('admin.user.update');
+            Route::delete('/{user}', [UserController::class, 'delete'])->name('admin.user.delete');
     });
     Route::group(['namespace' => 'category', 'prefix' => 'category'], function () {
         Route::get('/', [CategoryController::class, 'index'])->name('admin.category.index');
